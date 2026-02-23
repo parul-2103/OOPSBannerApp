d7198928ae4147f59ef802843cf5bead02061371
parul-2103 <parul.pinnu@gmail.com>
2026-02-23 23:53:35 +0530

    UC2: Banner using print statements

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * UC2 - Prints "OOPS" as a 7-line ASCII banner.
+ *
+ * @author Prachi Shaw
+ * @version 2.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     *  ***** ");
+        System.out.println("*     * *     * *  *  *       *");
+        System.out.println("*     * *     * *             *");
+        System.out.println(" *****   *****  *        ****** ");
+
+    }
+}
