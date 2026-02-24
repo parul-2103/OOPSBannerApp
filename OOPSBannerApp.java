b01ea9d95506a5aad943fe758b99e4f07b737b06
parul-2103 <parul.pinnu@gmail.com>
2026-02-24 10:39:13 +0530

    UC3: Refactor banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,40 @@
 /**
  * OOPSBannerApp
- * UC2 - Prints "OOPS" as a 7-line ASCII banner.
+ * UC3 - Prints "OOPS" banner using String.join()
  *
- * @author Prachi Shaw
- * @version 2.0
+ * Refactored from UC2 to improve memory efficiency.
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * * *****");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        String line1 = String.join("",
+                " *****  ", " *****  ", " *****  ", " ***** ");
 
+        String line2 = String.join("",
+                "*     * ", "*     * ", "*     * ", "*     *");
+
+        String line3 = String.join("",
+                "*     * ", "*     * ", "*     * ", "*     *");
+
+        String line4 = String.join("",
+                "*     * ", "*     * ", "*     * ", "* *****");
+
+        String line5 = String.join("",
+                "*     * ", "*     * ", "*     * ", "*     *");
+
+        String line6 = String.join("",
+                "*     * ", "*     * ", "*     * ", "*     *");
+
+        String line7 = String.join("",
+                " *****  ", " *****  ", " *****  ", " ***** ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
     }
 }
