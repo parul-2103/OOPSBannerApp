a0d03c394da67f1b556e16dcda611484b10ea505
parul-2103 <parul.pinnu@gmail.com>
2026-02-24 10:41:21 +0530

    UC4: Print OOPS banner using array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,40 +1,28 @@
 /**
  * OOPSBannerApp
- * UC3 - Prints "OOPS" banner using String.join()
+ * UC4 - Prints "OOPS" banner using Array & Loop
  *
- * Refactored from UC2 to improve memory efficiency.
+ * Improves UC3 by removing repetitive print statements
+ * and using a String array with enhanced for-loop.
+ *
+ * @version 4.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String line1 = String.join("",
-                " *****  ", " *****  ", " *****  ", " ***** ");
+        String[] bannerLines = {
+            String.join("", " *****  ", " *****  ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+            String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+            String.join("", "*     * ", "*     * ", "*     * ", "* *****"),
+            String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+            String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+            String.join("", " *****  ", " *****  ", " *****  ", " ***** ")
+        };
 
-        String line2 = String.join("",
-                "*     * ", "*     * ", "*     * ", "*     *");
-
-        String line3 = String.join("",
-                "*     * ", "*     * ", "*     * ", "*     *");
-
-        String line4 = String.join("",
-                "*     * ", "*     * ", "*     * ", "* *****");
-
-        String line5 = String.join("",
-                "*     * ", "*     * ", "*     * ", "*     *");
-
-        String line6 = String.join("",
-                "*     * ", "*     * ", "*     * ", "*     *");
-
-        String line7 = String.join("",
-                " *****  ", " *****  ", " *****  ", " ***** ");
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println(line6);
-        System.out.println(line7);
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
